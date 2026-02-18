7eb4b723830535bc62f15c0c850ce0e59868da27
Miketo <miketo.developer@gmail.com>
2026-02-18 00:03:14 -0600

    feat: inicialización de proyecto Angular 19 y conexión base con Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// Importaciones de Firebase
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    // Configuración funcional de Firebase
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
+
+
+
+/*
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAorJ49wZjgHSmYtTjnU9h_4825Nbh8Wfc",
+  authDomain: "edutareas-pwa.firebaseapp.com",
+  projectId: "edutareas-pwa",
+  storageBucket: "edutareas-pwa.firebasestorage.app",
+  messagingSenderId: "43994360130",
+  appId: "1:43994360130:web:fd7c8d95b49853e80340db"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+*/
